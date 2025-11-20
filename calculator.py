9117ecbbb216d4e67f157f122cc7f12d87babc17
mrLadejobi <ladejobioluwasaanumi@gmail.com>
2025-11-20 07:57:57 +0100

    Update and rename rock-paper-scissors.py to calculator.py

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def addition(a, b):
+    return a + b
+
+def mul_numbers(a, b):
+    return a * b
+
+def subtract_numbers(a, b):
+    return a - b
+
+def divide_numbers(a, b):
+    return a // b
+
+result = divide_numbers(20, 5)
+print(result)
+
+
+username = input("What is your name?")
+def say_hello(username):
+    return f"Hello {username}"
+
+
+response = say_hello(username)
+print(response)
+
+
+
+answer = addition(202020, 303030)
+print(answer)
+
+
+
+
+
+
